d16beeeb7975fd92ee4eb4f99e715259107b52e9
haminji <h.river771@gmail.com>
2020-07-17 14:54:19 +0900

    change xml image size

--- db_concat_v1.py
+++ db_concat_v1.py
@@ -4,6 +4,8 @@ import cv2
 from os import listdir
 from os.path import isfile, join
 
+import xml.etree.ElementTree as ET
+
 def concat_img (img_path, front_path, rear_path, left_path, right_path):
     front_img = cv2.imread(img_path + front_path + ".jpg")
     rear_img = cv2.imread(img_path + rear_path + ".jpg")
@@ -14,8 +16,34 @@ def concat_img (img_path, front_path, rear_path, left_path, right_path):
     left_right = cv2.hconcat([left_img, right_img])
     concat_all = cv2.vconcat([front_rear, left_right])
 
-    return concat_all 
-  
+    return concat_all
+
+def concat_ant (ant_path, front_path, rear_path, left_path, right_path):
+    front_tree = ET.parse(ant_path + front_path + ".xml")
+    rear_tree = ET.parse(ant_path + rear_path + ".xml")
+    left_tree = ET.parse(ant_path + left_path + ".xml")
+    right_tree = ET.parse(ant_path + right_path + ".xml")
+    
+    front_root = front_tree.getroot()
+    rear_root = rear_tree.getroot()
+    left_root = left_tree.getroot()
+    right_root = right_tree.getroot()
+    
+    size = _front_root.find("size")
+    new_width = int(size.find("width").text)*2
+    new_height = int(size.find("height").text)*2
+
+    # for ob in rear_root.iter("object"):
+    #     "xmin" "ymin" "xmax" "ymax"
+    # print country.tag
+
+
+
+
+
+
+
+
 db_path = "C:/Users/haminji/Documents/image_concat/GODTrain200618_SVM_case1_new/"
 img_path = db_path + "newJPEGImages/"
 ant_path = db_path + "newAnnotations/"
@@ -28,11 +56,13 @@ left_files = [f for f in allfiles if f.split("_")[6]=="left"]
 right_files = [f for f in allfiles if f.split("_")[6]=="right"]
 
 concat_imgs = []
+concat_ants = []
 
 check = 0
 for i in range(min(len(front_files), len(rear_files), len(left_files), len(right_files))):
     concat_imgs.append(concat_img(img_path, front_files[i], rear_files[i], left_files[i], right_files[i]))
-    #cv2.imshow('image', concat_imgs[i])
+    concat_ants.append(concat_ant(ant_path, front_files[i], rear_files[i], left_files[i], right_files[i]))
+
 
 
 
